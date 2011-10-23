50c2b973176e4130ebcbe5453ae58253582a1d0b
Piotrek Okoński <piotrek@okonski.org>
2011-10-23 20:08:42 +0200

    Czysty projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trekking::Application.config.secret_token = '0adf974b1cba293b2d2a4c420ae3ca1a2c67f348fad692c4428af143c92617fe48b832154e0f1cb7d8d0697dbcec753b95a468a2cc669507dbe8302e7e9d44ee'
