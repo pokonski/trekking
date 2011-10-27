1d7c19cf0063938353751f4d24e09d3ddc50d7df
Piotrek Okoński <piotrek@okonski.org>
2011-10-27 12:31:01 +0200

    Dodane zakladki i nowe assety

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require v3_epoly
-
+//= require bootstrap-tabs
 
 
 
--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module Trekking
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.01'
+    config.assets.version = '1.02'
   end
 end
