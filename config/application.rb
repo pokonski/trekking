823ea3a25d61595dc744007f9a88e4037aa6bbed
Piotrek Okoński <piotrek@okonski.org>
2011-10-27 03:06:02 +0200

    Nowa wersja statycznych

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module Trekking
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.01'
   end
 end
