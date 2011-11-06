865dc554accc7b24e713000f9dcf5b3c3d2454d7
Piotrek Okoński <piotrek@okonski.org>
2011-11-06 22:49:52 +0100

    initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -45,8 +45,9 @@ module Trekking
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.03'
+    config.assets.version = '1.04'
   end
 end
